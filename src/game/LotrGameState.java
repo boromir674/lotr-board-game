9e5e8894ba3fcd0dfdbf5c78395b3bc8bee175a3
Konstantinos Lampridis <k.lampridis@hotmail.com>
2016-10-23 13:41:51 +0200

    WIP

--- src/game/LotrGameState.java
+++ src/game/LotrGameState.java
@@ -1,5 +1,16 @@
 package game;
 
-public class LotrGameState {
+import java.util.Map;
 
+import enums.ActivitySymbol;
+
+public class LotrGameState {
+	
+	private byte activePlayerIndex;
+	private boolean ringON;
+	private byte mainCounter;
+	private Map<ActivitySymbol, Byte> secondaryActivityCounter;	
+	//private Map<ActivityCardType, Byte> hobbitCardsLeft;
+	
+	
 }
